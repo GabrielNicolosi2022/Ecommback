2baea9cc30c7d099cd828ebe3d6e84c3c6b45ae1
Gabriel Nicolosi <gabianp@live.com.ar>
2023-09-22 16:33:36 -0300

    Stripe dependency installation (payment gateway), updating config.js with stripe data.

--- src/config/config.js
+++ src/config/config.js
@@ -56,6 +56,11 @@ config.url = {
   recoverPassword: process.env.recoverPassword,
 };
 
+config.stripe = {
+  pk: process.env.PUBLIC_KEY,
+  sk: process.env.SECRET_KEY,
+};
+
 // console.log(config);
 
 export default config;

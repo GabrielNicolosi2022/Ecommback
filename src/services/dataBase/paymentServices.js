ca8b820397fd5cf2518ab3e00266d78b9a7e0080
Gabriel Nicolosi <gabianp@live.com.ar>
2023-09-23 13:35:32 -0300

    update  createPaymentIntent function

--- src/services/dataBase/paymentServices.js
+++ src/services/dataBase/paymentServices.js
@@ -4,11 +4,7 @@ import config from '../../config/config.js';
 const stripe = new Stripe(config.stripe.sk);
 
 // Intento de pago 
-const createPaymentIntent = async (data) => {
-  const result = await stripe.paymentIntents.create(data);
-  console.log(result.id);
-  return result;
-};
+const createPaymentIntent = async (data) => await stripe.paymentIntents.create(data)
 
 
 export {

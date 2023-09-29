acd105d050eec52e7919fd6d03278eab60db5845
Gabriel Nicolosi <gabianp@live.com.ar>
2023-09-29 16:27:31 -0300

    Update in main and cart views. Creation of payment router

--- src/router/Index.routes.js
+++ src/router/Index.routes.js
@@ -9,7 +9,7 @@ import userRouter from './users.routes.js';
 import productsRouter from './products.routes.js';
 import cartsRouter from './carts.routes.js';
 import loggerRouter from './logger.routes.js';
-
+import paymentRouter from './payment.routes.js';
 
 const indexRouter = Router();
 
@@ -20,5 +20,6 @@ indexRouter.use('/v1/api/users', userRouter);
 indexRouter.use('/v1/api/products', productsRouter);
 indexRouter.use('/v1/api/carts', cartsRouter);
 indexRouter.use('/v1/api/logger', loggerRouter);
+indexRouter.use('/v1/api/payment', paymentRouter);
 
 export default indexRouter;

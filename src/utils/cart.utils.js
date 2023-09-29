c85c8809394036426d1f595805bfcb7da7f02fd5
Gabriel Nicolosi <gabianp@live.com.ar>
2023-09-29 16:18:56 -0300

    creation of createCartTO, separateProductsByStock, processProducts functions and updating of calculateTotal and decimalToInteger functions.

--- src/utils/cart.utils.js
+++ src/utils/cart.utils.js
@@ -1,5 +1,25 @@
-export const calculateTotal = (processedProducts) => {
-  return processedProducts.reduce((total, product) => {
+import { getProductsById } from '../services/dataBase/prodServicesDB.js';
+import { toLocaleFloat } from './numbers.utils.js';
+import config from '../config/config.js';
+import { devLog, prodLog } from '../config/customLogger.js';
+let log;
+config.environment.env === 'production' ? (log = prodLog) : (log = devLog);
+
+export const createCartDTO = (products) => {
+  const cartDTO = products.map((cartItem) => ({
+    title: cartItem.product.title,
+    description: cartItem.product.description,
+    code: cartItem.product.code,
+    price: toLocaleFloat(cartItem.product.price),
+    status: cartItem.product.status,
+    stock: cartItem.product.stock,
+    quantity: cartItem.quantity,
+  }));
+  return cartDTO;
+};
+
+export const calculateTotal = (products) => {
+  return products.reduce((total, product) => {
     return total + product.product.price * product.quantity;
   }, 0);
 };
@@ -12,8 +32,50 @@ export const decimalToInteger = (value) => {
   }
 
   const integerString = valor.split('.');
-  const newIntegerString = integerString.join('')
+  const newIntegerString = integerString.join('');
   const integerValue = parseInt(newIntegerString);
 
   return integerValue;
+};
+
+export const separateProductsByStock = async (cart) => {
+  const productsToProcess = [];
+  const productsNotProcessed = [];
+
+  // Validar el stock de los productos en el carrito y separar los productos sin stock
+  for (const product of cart.products) {
+    const productId = product.product;
+    const quantityInCart = product.quantity;
+    // Obtener el producto por ID desde la base de datos
+    const productFromDB = await getProductsById(productId);
+    if (!productFromDB) {
+      productsNotProcessed.push(productId);
+      log.error(
+        `Producto con id ${productId._id} no encontrado en la base de datos`
+      );
+    } else if (productFromDB.stock >= quantityInCart) {
+      productsToProcess.push({ productFromDB, quantityInCart });
+      log.info(
+        `${quantityInCart} ud. del producto con id ${productId._id} serán procesadas.`
+      );
+    } else {
+      productsNotProcessed.push({ productFromDB, quantityInCart });
+      log.warn(`Producto con id ${productId._id} no tiene suficiente stock`);
+    }
+  }
+
+  return { productsToProcess, productsNotProcessed };
+};
+
+export const processProducts = (productsArray) => {
+  const processedProducts = [];
+
+  for (const { productFromDB, quantityInCart } of productsArray) {
+    processedProducts.push({
+      product: productFromDB,
+      quantity: quantityInCart,
+    });
+  }
+
+  return processedProducts;
 };
